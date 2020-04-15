d5eb74de94d7e8ab68a9c3d1336747a1ca143f39
jiejie168 <41291332+jiejie168@users.noreply.github.com>
2020-04-15 12:11:20 +0200

    1304

--- TrapRainWater.py
+++ TrapRainWater.py
@@ -12,15 +12,15 @@ Output: 6
 class Solution:
     def trap(self, height) -> int:
         # naive algorithm
-        # search the left max height of an element the right max height of an element,
+        # search the left max height of an element the right max height of an element,compared with itself!!!!.
         #find the maximum level of water it can trap after the rain,
         # which is equal to the minimum of maximum height of bars on both the sides minus its own height.
         n=len(height)
         result=0
         for i in range(1,n):
             ##
-            lmax=height[i] # this is important, can not be set to 0
-            rmax=height[i]
+            lmax=height[i] #the adjacent elements should be larger than this element ifself, can then can be store water.
+            rmax=height[i]  #this is important, can not be set to 0
             for j in range(i):
                 lmax=max(lmax,height[j])
             for k in range(i+1,n):
@@ -51,25 +51,26 @@ class Solution:
         return water
 
     def trap_twoPointer(self,height):
-        ## two pointers.
-        if height is None or len(height)<3:
-            return 0
+        # two pointers,  time complexity : O(n), space complexity : O(1)
         n=len(height)
-        i=0
-        j=n-1
-        level=water=0
-        while i<j:
-            it=height[i if height[i]<height[j] else j]
-            if height[i]<height[j]:
-                i+=1
+        if n==0:
+            return 0
+        l=0
+        r=n-1
+        lmax=height[l]
+        rmax=height[r]
+        result=0
+        while l<r:
+            # the short height in either left or right side determines the water units.
+            if lmax<rmax:
+                result+=lmax-height[l]
+                l+=1
+                lmax=max(lmax,height[l])
             else:
-                j-=1
-            if level>it:
-                water+=level-it
-            else:
-                level=it
-        return water
-
+                result+=rmax-height[r]
+                r-=1
+                rmax=max(rmax,height[r])
+        return result
 
 height=[0,1,0,2,1,0,1,3,2,1,2,1]
 solution=Solution()
